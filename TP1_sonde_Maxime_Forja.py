7c672f490e0466be2440186ff486dfbf35966d6f
maximef69 <forjamaxime@gmail.com>
2024-09-12 16:25:14 +0200

    add comment

--- TP1_sonde_Maxime_Forja.py
+++ TP1_sonde_Maxime_Forja.py
@@ -3,7 +3,7 @@ xa = float(input("Entrer la température de A"))
 ya = float(input("Entrer la résistance de A"))
 print("Entrer les valeurs du point B")
 xb = float(input("Entrer la température de B"))
-yb = float(input("Entrer la résitance de B"))
-A = (yb-ya)/(xb-xa)
-B = (ya-A*xa)
+yb = float(input("Entrer la résistance de B"))
+A = (yb-ya)/(xb-xa)                            #calcul du coefficient directeur
+B = (ya-A*xa)                                  #calcul de l'ordonnée à l'origine
 print(round(A,2),round(B,2))
