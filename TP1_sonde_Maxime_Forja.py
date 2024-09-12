949d98c1f0a3e93976f0b3ce030ca4d1cb344fd1
maximef69 <forjamaxime@gmail.com>
2024-09-12 16:59:09 +0200

    add division par 0

--- TP1_sonde_Maxime_Forja.py
+++ TP1_sonde_Maxime_Forja.py
@@ -1,9 +1,12 @@
 print("Entrer les valeurs du point A")
-xa = float(input("Entrer la température de A"))
-ya = float(input("Entrer la résistance de A"))
-print("Entrer les valeurs du point B")
-xb = float(input("Entrer la température de B"))
-yb = float(input("Entrer la résistance de B"))
+xa = float(input("Entrer la température de A : "))
+ya = float(input("Entrer la résistance de A : "))
+print("Entrer les valeurs du point B ")
+xb = float(input("Entrer la température de B : "))
+yb = float(input("Entrer la résistance de B : "))
+if xb-xa == 0 :
+    print("Vérifier les températures saisies (elles ne doivent pas être égale)") #pas de division par 0
+    exit()
 A = (yb-ya)/(xb-xa)                            #calcul du coefficient directeur
-B = (ya-A*xa)                                  #calcul de l'ordonnée à l'origine
-print(round(A,2),round(B,2))
+B = ya-A*xa                                    #calcul de l'ordonnée à l'origine
+print("La valeur de A =", round(A,2), "La valeur de B =", round(B,2))
